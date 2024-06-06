bf0a1cb2ef607ef176fccba538e21a71f2e8a1fd
Ramazan <49938340+trevochka-rmz@users.noreply.github.com>
2024-06-06 14:01:52 +0700

    Update helper.sql

--- helper.sql
+++ helper.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июн 04 2024 г., 17:27
+-- Время создания: Июн 06 2024 г., 10:01
 -- Версия сервера: 5.7.38
 -- Версия PHP: 7.1.33
 
@@ -30,9 +30,76 @@ SET time_zone = "+00:00";
 CREATE TABLE `food_drinks` (
   `id_food` int(11) NOT NULL,
   `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `price` int(11) DEFAULT NULL,
-  `count` int(11) DEFAULT NULL
+  `price` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `food_drinks`
+--
+
+INSERT INTO `food_drinks` (`id_food`, `name`, `price`) VALUES
+(0, 'Чай', 50),
+(1, 'Кола', 150),
+(2, 'Морс', 50),
+(3, 'Салат', 250),
+(4, 'Суп', 250),
+(5, 'Сэндвич', 150);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order_food`
+--
+
+CREATE TABLE `order_food` (
+  `id_order` int(11) NOT NULL,
+  `id_food` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `counts` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `order_food`
+--
+
+INSERT INTO `order_food` (`id_order`, `id_food`, `id_user`, `counts`) VALUES
+(6, 0, 0, 1),
+(7, 0, 0, 2),
+(8, 3, 0, 1),
+(9, 1, 0, 1),
+(10, 4, 0, 1),
+(11, 0, 0, 3),
+(12, 0, 0, 1),
+(13, 0, 0, 2),
+(14, 1, 0, 1),
+(15, 0, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order_services`
+--
+
+CREATE TABLE `order_services` (
+  `id_order` int(11) NOT NULL,
+  `id_services` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `counts` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `order_services`
+--
+
+INSERT INTO `order_services` (`id_order`, `id_services`, `id_user`, `counts`) VALUES
+(7, 0, 0, 1),
+(8, 3, 0, 1),
+(9, 4, 0, 1),
+(10, 2, 0, 2),
+(11, 2, 0, 2),
+(12, 0, 0, 1),
+(13, 4, 0, 1),
+(14, 5, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -43,8 +110,20 @@ CREATE TABLE `food_drinks` (
 CREATE TABLE `services` (
   `id_services` int(11) NOT NULL,
   `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `count_services` int(11) DEFAULT NULL
+  `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `services`
+--
+
+INSERT INTO `services` (`id_services`, `name`, `price`) VALUES
+(0, 'Подушка', 300),
+(1, 'Одеяло', 500),
+(2, 'Приборы', 100),
+(3, 'Наушники', 900),
+(4, 'Телефон', 600),
+(5, 'Проводник', 0);
 
 -- --------------------------------------------------------
 
@@ -53,10 +132,19 @@ CREATE TABLE `services` (
 --
 
 CREATE TABLE `user_helper` (
-  `id` int(11) NOT NULL,
-  `id_food` int(11) DEFAULT NULL,
-  `id_services` int(11) DEFAULT NULL
+  `id_user` int(11) NOT NULL,
+  `report` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `user_helper`
+--
+
+INSERT INTO `user_helper` (`id_user`, `report`) VALUES
+(0, 'Классный сервис, все понравилось'),
+(1, 'Отличный сервис, все понравилось!'),
+(2, 'Супер'),
+(3, '4/5 сервис неплохой');
 
 --
 -- Индексы сохранённых таблиц
@@ -69,6 +157,22 @@ ALTER TABLE `food_drinks`
   ADD PRIMARY KEY (`id_food`);
 
 --
+-- Индексы таблицы `order_food`
+--
+ALTER TABLE `order_food`
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `id_food` (`id_food`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Индексы таблицы `order_services`
+--
+ALTER TABLE `order_services`
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `id_services` (`id_services`),
+  ADD KEY `id_user` (`id_user`);
+
+--
 -- Индексы таблицы `services`
 --
 ALTER TABLE `services`
@@ -78,20 +182,41 @@ ALTER TABLE `services`
 -- Индексы таблицы `user_helper`
 --
 ALTER TABLE `user_helper`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_food` (`id_food`),
-  ADD KEY `id_services` (`id_services`);
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `order_food`
+--
+ALTER TABLE `order_food`
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT для таблицы `order_services`
+--
+ALTER TABLE `order_services`
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
 
 --
--- Ограничения внешнего ключа таблицы `user_helper`
+-- Ограничения внешнего ключа таблицы `order_food`
 --
-ALTER TABLE `user_helper`
-  ADD CONSTRAINT `user_helper_ibfk_1` FOREIGN KEY (`id_food`) REFERENCES `food_drinks` (`id_food`),
-  ADD CONSTRAINT `user_helper_ibfk_2` FOREIGN KEY (`id_services`) REFERENCES `services` (`id_services`);
+ALTER TABLE `order_food`
+  ADD CONSTRAINT `order_food_ibfk_1` FOREIGN KEY (`id_food`) REFERENCES `food_drinks` (`id_food`),
+  ADD CONSTRAINT `order_food_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user_helper` (`id_user`);
+
+--
+-- Ограничения внешнего ключа таблицы `order_services`
+--
+ALTER TABLE `order_services`
+  ADD CONSTRAINT `order_services_ibfk_1` FOREIGN KEY (`id_services`) REFERENCES `services` (`id_services`),
+  ADD CONSTRAINT `order_services_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user_helper` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
